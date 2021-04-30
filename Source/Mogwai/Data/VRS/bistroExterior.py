28cfaa807d56f2f3a6026d4672304c42df8c43a2
Jaliborc <jaliborc@arenajunkies.com>
2021-04-30 19:57:45 +0100

    BistroExterior working with new fscene

--- Source/Mogwai/Data/VRS/bistroExterior.py
+++ Source/Mogwai/Data/VRS/bistroExterior.py
@@ -2,10 +2,12 @@ from Data.VRS.graphs import *
 import os
 
 # Scene
-m.loadScene(os.getenv('FALCOR_MEDIA_FOLDERS').strip("/") + '/Bistro/BistroExterior.fbx')
+p = os.getenv('FALCOR_MEDIA_FOLDERS').strip('/')
+m.loadScene(p + '/Bistro/BistroExterior.fscene')
 m.scene.renderSettings = SceneRenderSettings(useEnvLight=True, useAnalyticLights=True, useEmissiveLights=True)
-#m.scene.setEnvMap('C:/Users/jaliborc/Documents/Coding/vrs/data/scenes/SunTemple/SunTemple_Skybox.jpg')
-m.scene.camera.nearPlane = 0.1
+m.scene.setEnvMap(p + '/Bistro/san_giuseppe_bridge_4k.hdr')
+m.scene.getLight(0).intensity = 10.0
+m.scene.camera.nearPlane = 1
 m.scene.camera.farPlane = 800.0
 m.addGraph(captureGraph())
 m.addGraph(vrsGraph())
