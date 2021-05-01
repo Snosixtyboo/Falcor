0798b992d732196c5c8bab5f1469a65ea79ae68b
Jaliborc <jaliborc@arenajunkies.com>
2021-05-01 01:42:36 +0100

    Added skybox to bistro interior, updated TAA

--- Source/Mogwai/Data/VRS/bistro.py
+++ Source/Mogwai/Data/VRS/bistro.py
@@ -2,8 +2,10 @@ from Data.VRS.graphs import *
 import os
 
 # Scene
-m.loadScene(os.getenv('FALCOR_MEDIA_FOLDERS').strip("/") + '/Bistro/BistroInterior.fbx')
+p = os.getenv('FALCOR_MEDIA_FOLDERS').strip('/')
+m.loadScene(p + '/Bistro/BistroInterior.fbx')
 m.scene.renderSettings = SceneRenderSettings(useEnvLight=True, useAnalyticLights=True, useEmissiveLights=True)
+m.scene.setEnvMap(p + '/Bistro/san_giuseppe_bridge_4k.hdr')
 m.scene.camera.nearPlane = 0.10000000149011612
 m.scene.camera.farPlane = 100.0
 m.scene.cameraSpeed = 1.0
